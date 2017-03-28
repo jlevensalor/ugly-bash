4614f523d1e3f48153393848b7e6046d66ded7ae
Jerrett Levensalor <jlevensalor@gmail.com>
2017-03-27 19:47:26 -0700

    Fixing for mac

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ugly-bash
 Really quick and ugly way to save time setting up a terraform and packer on Linux or Mac....if it's windows who cares. 
+
+This will put the files /opt/local/<package>/version/
+You will need to run this as sudo though 
+
+###For example: 
+``` 
+$ sudo bash setup-terraform-packer-mac-linux.sh 
+``` 
+###If you want to see details 
+
+``` 
+$ sudo bash -x setup-terraform-packer-mac-linux.sh
+``` 
--- setup-terraform-packer-mac-linux.sh
+++ setup-terraform-packer-mac-linux.sh
@@ -14,41 +14,52 @@ cd /opt/local/terraform/$TV
 #Check for machine type
 # Download Terraform. URI: https://www.terraform.io/downloads.html
 
-if [ ${MT} == 'i686' ]; then
+if [ "${MT}" == "i686" ] && [ "${MOS}" == "Linux" ]; then
+printf  "This is 32-bit Linux\n"
+printf "So we are installing the Linux 32 bit binary\n"
 curl -O https://releases.hashicorp.com/terraform/{$TV}/terraform_{$TV}_linux_386.zip 
 unzip terraform*.zip
 
-elif [ ${MT} == 'x86_64' ]; then
+elif [ "${MT}" == "x86_64" ] && [ "${MOS}" == "Linux" ]; then
+printf "This is a 64-bit Linux\n"
+printf "So we are installing the Linux 64 bit binary\n"
 curl -O https://releases.hashicorp.com/terraform/{$TV}/terraform_{$TV}_linux_amd64.zip 
-#unzip terraform_*_linux_amd64.zip
 unzip terraform*.zip
 
 elif [ "${MOS}" == "Darwin" ]; then
+printf "This looks like a Mac\n"
+printf "So we are installing the Darwin binary\n"
 curl -O https://releases.hashicorp.com/terraform/{$TV}/terraform_{$TV}_darwin_amd64.zip
-#unzip terraform_*_darwin_amd64.zip
 unzip terraform*.zip
 
 else 
-"These are the OS types you should use...."
+printf "OS type not found....\n"
 fi
 
 cd /opt/local/packer/$PV
 
 # Download Packer. URI: https://www.packer.io/downloads.html
-if [ ${MT} == 'i686' ]; then
+
+if [ "${MT}" == "i686" ] && [ "${MOS}" == "Linux" ]; then
+echo "This is 32-bit Linux"
+printf "So we are installing the Linux 32 bit binary\n"
 curl -O https://releases.hashicorp.com/packer/{$PV}/packer_{$PV}_linux_386.zip 
 unzip packer_*_linux_386.zip
 
-elif [ ${MT} == 'x86_64' ]; then
+elif [ "${MT}" == "x86_64" ] && [ "${MOS}" == "Linux" ]; then
+printf "This is a 64-bit Linux\n"
+printf "So we are installing the Linux 64 bit binary\n"
 curl -O https://releases.hashicorp.com/packer/{$PV}/packer_{$PV}_linux_amd64.zip 
 unzip packer_*_linux_amd64.zip
 
 elif [ "${MOS}" == "Darwin" ]; then
-curl -O https://releases.hashicorp.com/packer/{$PV}/packer_{$PV}_darwin_amd64.zip
+printf "This looks like a Mac\n"
+printf "So we are installing the Darwin binary\n"
+curl -O https://releases.hashicorp.com/packer/{$PV}/packer_{$PV}_darwin_amd64.zip 
 unzip packer_*_darwin_amd64.zip
 
 else 
-"These are the OS types you should use...."
+printf "OS type not found....\n"
 fi
 
 echo '
@@ -59,9 +70,10 @@ export PATH=/opt/local/terraform/'$TV'/:/opt/local/packer/'$PV'/:$PATH
 source ~/.bash_profile
 
 #Remove the zips 
-rm /opt/local/*/*/*.zip 
+rm /opt/local/terraform/*/*.zip 
+rm /opt/local/packer/*/*.zip 
 
 # verify we're all set to terraform and packer.
+printf "Lets see if the binary's work by running them\n"
 terraform
 packer
-
